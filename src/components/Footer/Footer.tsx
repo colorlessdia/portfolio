82c5cfb86102458873dc46afad935b730a0bf2f5
colorlessdia <colorlessdia@gmail.com>
2024-05-19 03:14:14 +0900

    footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer_inner}>
+
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
