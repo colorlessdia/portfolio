cf0d6f04a834d9e4d26d9086401ef9e9b831b240
colorlessdia <colorlessdia@gmail.com>
2024-05-19 00:06:51 +0900

    add module css

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="react-scripts" />
+
+// declaration: *.module.css
+declare module "*.module.css" {
+  const classes: { [selector: string]: string };
+
+  export default classes;
+}
