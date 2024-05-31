ffc47a4656f0abfff5f72cbce0d8c3ab359b0368
colorlessdia <colorlessdia@gmail.com>
2024-06-01 00:28:33 +0900

    browser router & base url

---
+++ src/libs/router/router.tsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+// import: router pages
+import RootPage from '../../pages/RootPage/RootPage';
+import MainPage from '../../pages/MainPage/MainPage';
+import MemoPage from '../../pages/MemoPage/MemoPage';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootPage />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: '/memo',
+        element: <MemoPage />,
+      }
+    ],
+  }
+], { basename: process.env.PUBLIC_URL });
+
+export default router;
