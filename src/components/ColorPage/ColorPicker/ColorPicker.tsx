0ac62020c4f8e00fceef8608f0de038f1393d78c
colorlessdia <colorlessdia@gmail.com>
2024-06-07 02:58:30 +0900

    modify style

--- src/components/ColorPage/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPage/ColorPicker/ColorPicker.tsx
@@ -43,22 +43,24 @@ const ColorPicker = () => {
 
   return (
     <div className={styles.color_picker_area}>
-      <input
-        type='color'
-        title='색상을 선택하세요'
-        value={colorValue}
-        onChange={handleChangeColorValue}
-        className={`${styles.color_picker} ${theme === 'dark' ? 'dark_bg_02' : ''}`}
-      />
-      <button
-        type='button'
-        title='선택한 색상이 등록됩니다'
-        onClick={handleClickAddColor}
-        className={`${styles.pick_button} ${theme === 'dark' ? 'dark_bg_02' : ''}`}
-      >
-        색상 등록
-      </button>
-      <RemoveAllButton />
+      <div className={styles.button_group}>
+        <input
+          type='color'
+          title='색상을 선택하세요'
+          value={colorValue}
+          onChange={handleChangeColorValue}
+          className={`${styles.color_picker} ${theme === 'dark' ? 'dark_bg_02' : ''}`}
+        />
+        <button
+          type='button'
+          title='선택한 색상이 등록됩니다'
+          onClick={handleClickAddColor}
+          className={`${styles.pick_button} ${theme === 'dark' ? 'dark_bg_02' : ''}`}
+        >
+          색상 등록
+        </button>
+        <RemoveAllButton />
+      </div>
       <p className={styles.message}>{message}</p>
     </div>
   );
