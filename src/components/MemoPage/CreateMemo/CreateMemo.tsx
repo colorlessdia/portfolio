451d23cde51f0115610b4fe134acc5d69db0b678
colorlessdia <colorlessdia@gmail.com>
2024-06-09 16:20:08 +0900

    modify style

--- src/components/MemoPage/CreateMemo/CreateMemo.tsx
+++ src/components/MemoPage/CreateMemo/CreateMemo.tsx
@@ -11,24 +11,26 @@ const CreateMemo = () => {
   } = useCreateMemo();
 
   return (
-    <div className={styles.create_memo}>
-      <input
-        type='text'
-        ref={inputRef}
-        value={memoValue}
-        onChange={handleChangeCreateMemoValue}
-        onKeyDown={handleKeyDownCreateMemo}
-        placeholder='메모를 입력해보세요'
-        className={styles.input}
-      />
-      <button
-        type='button'
-        title='입력한 메모가 등록됩니다'
-        onClick={handleClickCreateMemo}
-        className={styles.button}
-      >
-        메모 생성
-      </button>
+    <div className={styles.create_memo_wrap}>
+      <div className={styles.create_memo}>
+        <input
+          type='text'
+          ref={inputRef}
+          value={memoValue}
+          onChange={handleChangeCreateMemoValue}
+          onKeyDown={handleKeyDownCreateMemo}
+          placeholder='메모를 입력해보세요'
+          className={styles.input}
+        />
+        <button
+          type='button'
+          title='입력한 메모가 등록됩니다'
+          onClick={handleClickCreateMemo}
+          className={styles.button}
+        >
+          메모 생성
+        </button>
+      </div>
     </div>
   );
 };
